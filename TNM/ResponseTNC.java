f7d12913b739558a958bb4ed1708860c2e2de473
Chris <cep38@pitt.edu>
2013-04-22 00:11:00 -0400

    TNM changes:
    - java code convention changes
    - changes to use actual track model

--- TNM/ResponseTNC.java
+++ TNM/ResponseTNC.java
@@ -1,7 +1,17 @@
+/**
+ * NSECS-TNM & TNC
+ * 
+ * This file contains the main container class which holds the response from a 
+ *   given train's controller after each timeTick.
+ * 
+ * @author Chris Paskie 
+ * @version 04/25/2013
+*/
+
+
 package TNM;
 
-public class ResponseTNC
-{
+public class ResponseTNC {
 	public double powerCommand;
 	public boolean issetServiceBrake;
 	public boolean issetEmerBrake;
@@ -10,8 +20,9 @@ public class ResponseTNC
 	public double targetTemperatureTNC;
 	public String announcement;
 	
-	public ResponseTNC(double powerCommand, boolean issetServiceBrake, boolean issetEmerBrake, boolean issetLightsOn, boolean issetDoorsOpen, double targetTemperatureTNC, String announcement)
-	{
+	public ResponseTNC(double powerCommand, boolean issetServiceBrake, boolean issetEmerBrake, 
+			boolean issetLightsOn, boolean issetDoorsOpen, double targetTemperatureTNC, 
+			String announcement) {
 		this.powerCommand = powerCommand;
 		this.issetServiceBrake = issetServiceBrake;
 		this.issetEmerBrake = issetEmerBrake;
